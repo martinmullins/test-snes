98754e0f7e0e5eef3f614b467219880f48a2bf8c
Martin Mullins <martin.mullins942+git@gmail.com>
2022-10-05 16:01:30 -0400

    deploy Wed Oct  5 04:01:30 PM EDT 2022

---
+++ _next/static/zapoOoqfMI6rn6jat4ZVF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-de075af84e4023f8.js","static/chunks/0ac4af9d-c640a3b3c86c54e6.js","static/chunks/pages/index-a73268be6d56e83d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
